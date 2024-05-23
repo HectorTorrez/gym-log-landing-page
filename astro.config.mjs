0618cf1fd856fc88a591a1cdf3513e195f47a7c9
Hector Torrez <torrezhectorb@gmail.com>
2024-05-22 20:22:25 -0600

    fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: `https://www.gymLog-lading-page.com`,
+  output: "static",
+  outDir: "./docs",
+  build: {
+    assets: "astro",
+  },
 });
